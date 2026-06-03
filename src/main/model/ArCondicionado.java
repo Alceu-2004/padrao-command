38453ec2d42c009362ded2f9f3df4d53d222ed57
Alceu Vasconcellos Botelho dos Reis <thetecworld17@gmail.com>
2026-06-03 14:47:34 -0300

    feat: adiciona receptor de ar-condicionado

---
+++ src/main/model/ArCondicionado.java
@@ -0,0 +1,18 @@
+package model;
+
+public class ArCondicionado {
+
+    public void ligar() {
+
+        System.out.println(
+                "Ar-condicionado ligado"
+        );
+    }
+
+    public void desligar() {
+
+        System.out.println(
+                "Ar-condicionado desligado"
+        );
+    }
+}
