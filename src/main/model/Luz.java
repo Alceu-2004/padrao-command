5e6d700428d45fb3c483e4f28bb7e7c67fc1b54a
Alceu Vasconcellos Botelho dos Reis <thetecworld17@gmail.com>
2026-06-03 14:46:35 -0300

    feat: adiciona receptor de luz

---
+++ src/main/model/Luz.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Luz {
+
+    public void ligar() {
+
+        System.out.println(
+                "Luz ligada"
+        );
+    }
+
+    public void desligar() {
+
+        System.out.println(
+                "Luz desligada"
+        );
+    }
+}
